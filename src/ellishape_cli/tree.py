bc5b69002a915b5351f303fb35379c6f27d316ff
Ping Wu <wpwupingwp@outlook.com>
2025-01-12 16:18:02 +0800

    output distance matrix

--- src/ellishape_cli/tree.py
+++ src/ellishape_cli/tree.py
@@ -44,7 +44,7 @@ def read_csv(input_file: Path, simple_name=True):
                 log.warning(f'Found duplicate names: {i} -> {simple}')
                 raise SystemExit(-2)
             s2.add(simple)
-            simple_names.append(i)
+            simple_names.append(simple)
     else:
         simple_names = names
     # names = np.array(data)[:, 0:1].flatten().tolist()
@@ -53,15 +53,17 @@ def read_csv(input_file: Path, simple_name=True):
     return simple_names, data
 
 
-def matrix2csv(out_file: Path, names:list, dist_dict: dict) -> Path:
-    # output matrix to csv file
-    # todo
-    e_dist_matrix, h_dist_matrix, s_dist_matrix = [], [], []
-    for i in range(len(names)):
-        e_dist_list, h_dist_list, s_dist_list = [], [], []
-        for j in range(i+1):
-            pass
-    pass
+def matrix2csv(out_file: Path, names:list, matrix: list) -> Path:
+    with open(out_file, 'w', newline='') as csv_file:
+        writer = csv.writer(csv_file)
+        header = ['Name', ]
+        header.extend(names)
+        writer.writerow(header)
+        for name, row in zip(names, matrix):
+            line = [name,]
+            line.extend(row)
+            writer.writerow(line)
+    return out_file
 
 
 def get_distance(a: np.array, b: np.array, skip_s_dist=True):
@@ -73,7 +75,6 @@ def get_distance(a: np.array, b: np.array, skip_s_dist=True):
         s_dist = 0
     else:
         s_dist = s_calc.computeDistance(a, b)
-    print(e_dist, h_dist, s_dist)
     return e_dist, h_dist, s_dist
 
 def get_distance_matrix(names, data):
@@ -94,7 +95,7 @@ def get_distance_matrix(names, data):
             # log.info(f'{data[i][0]} {data[j][0]}')
             log.info(f'{a_name} {b_name}')
             log.debug(f'{a.shape=} {b.shape=}')
-            log.info(f'{e_dist=:.2f} {h_dist=:.2f} {s_dist=:.2f}')
+            log.debug(f'{e_dist=:.2f} {h_dist=:.2f} {s_dist=:.2f}')
             e_dist_list.append(e_dist)
             h_dist_list.append(h_dist)
             s_dist_list.append(s_dist)
@@ -103,7 +104,6 @@ def get_distance_matrix(names, data):
         s_dist_matrix.append(s_dist_list)
         # data = np.array(list(reader))
         # distance_matrix_full = np.array(list(reader))
-    print('e_dist, h_dist, s_dist')
     # from itertools import chain
     # e_ = list(chain.from_iterable(e_dist_matrix))
     # h_ = list(chain.from_iterable(h_dist_matrix))
@@ -126,7 +126,9 @@ def build_nj_tree(names: list, matrix: list) -> 'Phylo.Tree':
         if t.branch_length < 0:
             log.debug(f'{t.branch_length:.2f}')
         t.branch_length = abs(t.branch_length)
-    log.info(tree)
+    # for t in tree.get_nonterminals():
+    #     t.name = ''
+    log.debug(tree)
 
     # Phylo.draw(tree, branch_labels=lambda x: f'{x.branch_length:.2f}', do_show=True)
     # plt.savefig(name + '.pdf')
@@ -137,22 +139,26 @@ def get_tree():
     # init args
     log.info('Start')
     arg = parse_args()
-    arg.input = Path(arg.input).absolute()
+    arg.input = Path(arg.input).absolute().resolve()
     assert arg.input.exists()
 
     names, data = read_csv(arg.input)
+    log.info(f'{len(names)} samples')
+    log.info(f'Need {len(data)*(len(data)-1)/2} times comparison')
     if len(names) == 0:
         log.error('Empty input')
         raise SystemExit(-1)
 
     e_dist_matrix, h_dist_matrix, s_dist_matrix = get_distance_matrix(names, data)
-    for name, matrix in zip(['e_dist', 'h_dist', 's_dist'],
+    for m_name, matrix in zip(['e_dist', 'h_dist', 's_dist'],
                             [e_dist_matrix, h_dist_matrix, s_dist_matrix]):
-        out_tree = arg.input.parent / (arg.output+name+'.nwk')
-        out_matrix = arg.input.parent / (arg.output+name+'.matrix.csv')
+        out_tree = arg.input.parent / f'{arg.output}-{m_name}.nwk'
+        out_matrix = arg.input.parent / f'{arg.output}-{m_name}.matrix.csv'
         tree = build_nj_tree(names, matrix)
         Phylo.write(tree, out_tree, 'newick')
-        # matrix2csv(out_matrix, matrix)
+        matrix2csv(out_matrix, names, matrix)
+        log.info(f'Output tree {out_tree}')
+        log.info(f'Output matrix {out_matrix}')
     log.info('Done')
 
 
