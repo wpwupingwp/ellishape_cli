eb8ff376ec85c64a1d40e70cc42fc762d1352762
Ping Wu <wpwupingwp@outlook.com>
2025-01-20 15:46:10 +0800

    check key missing before run

--- src/ellishape_cli/tree.py
+++ src/ellishape_cli/tree.py
@@ -35,9 +35,9 @@ def parse_args():
     return arg.parse_args()
 
 
-# df = pd.read_excel(distance_csv)
-# print(df)
 def read_csv(input_file: Path, simple_name=True):
+    # read input csv
+    # sample name, values
     raw_data = np.loadtxt(input_file, delimiter=',', dtype=str, quotechar='"')
     data = raw_data[1:, 1:]
     names = raw_data[1:, 0:1].flatten().tolist()
@@ -56,8 +56,31 @@ def read_csv(input_file: Path, simple_name=True):
     return simple_names, data
 
 
-def x(name, a, b):
-    pass
+def read_kind_csv(category_csv: Path, sample_names: list) -> (dict, list):
+    name_kind = dict()
+    with open(category_csv, 'r', encoding='utf-8') as f:
+        next(f)
+        for line in f:
+            name, kind = line.rstrip().split(',')
+            name_kind[name] = kind
+    kinds_set = set(name_kind.values())
+    kinds = sorted(list(kinds_set))
+    # check missing
+    missing1 = set(sample_names).difference(set(name_kind.keys()))
+    missing2 = set(name_kind.keys()).difference(set(sample_names))
+    if len(missing1) > 0:
+        log.warning(f'{len(missing1)} found in sample list but not in kind list')
+        log.info(f'{"\n".join(missing1)}')
+        log.critical('Continue?')
+        if not input().lower().startswith('y'):
+            raise SystemExit(-3)
+    if len(missing2) > 0:
+        log.warning(f'{len(missing2)} found in kind list but not in sample list')
+        log.info(f'{"\n".join(missing2)}')
+        log.critical('Continue?')
+        if not input().lower().startswith('y'):
+            raise SystemExit(-3)
+    return name_kind, kinds
 
 
 def matrix2csv(m_name: str, names:list, matrix: list, arg) -> Path|None:
@@ -101,8 +124,6 @@ def get_distance(a_name: str, b_name: str, a_raw: np.array, b_raw: np.array,
     # log.debug(f'{e_dist=:.2f} {h_dist=:.2f} {s_dist=:.2f}')
     log.info(f'{pair_name} done')
     return pair_name, e_dist, h_dist, s_dist
-
-
 
 
 def get_distance_matrix(names, data, arg):
@@ -191,32 +212,17 @@ def build_nj_tree(m_name, names: list, matrix: list, arg):
     return tree
 
 
-def matrix_to_kinds(sample_names: list, matrix: list, category_csv: Path):
-    check_input(category_csv)
-    name_kind = dict()
-    with open(category_csv, 'r', encoding='utf-8') as f:
-        next(f)
-        for line in f:
-            name, kind = line.rstrip().split(',')
-            name_kind[name] = kind
-
-    kinds_set = set(name_kind.values())
-    kinds = sorted(list(kinds_set))
-
+def matrix_to_kinds(sample_names: list, matrix: list,
+                    name_kind: dict, kinds: list):
     kind_values = defaultdict(list)
-    # for i in range(len(kinds)):
-    #     # mean_list, std_list = [], []
-    #     for j in range(len(kinds)):
-    #         a_name = kinds[i]
-    #         b_name = kinds[j]
-    #         pair_name = f'{a_name}-{b_name}'
-    #         kind_values[pair_name] = []
-    # kind_matrix.append(line_value)
     for x in range(len(sample_names)):
         for y in range(x+1):
-            x_kind = name_kind[sample_names[x]]
-            y_kind = name_kind[sample_names[y]]
-            value = matrix[x][y]
+            x_kind = name_kind.get(sample_names[x], 'Unknown')
+            y_kind = name_kind.get(sample_names[y], 'Unknown')
+            if x_kind == 'Unknown' or y_kind == 'Unknown':
+                value = 0
+            else:
+                value = matrix[x][y]
             if x_kind < y_kind:
                 x_kind, y_kind = y_kind, x_kind
             kind_values[f'{x_kind}-{y_kind}'].append(value)
@@ -234,9 +240,7 @@ def matrix_to_kinds(sample_names: list, matrix: list, category_csv: Path):
             std_list.append(np.std(values))
         kind_mean_matrix.append(mean_list)
         kind_std_matrix.append(std_list)
-    print(kind_mean_matrix)
-    print(kind_std_matrix)
-    return kinds, kind_mean_matrix, kind_std_matrix
+    return kind_mean_matrix, kind_std_matrix
 
 
 def get_tree():
@@ -246,11 +250,15 @@ def get_tree():
     arg = parse_args()
     arg.input = Path(arg.input).absolute().resolve()
     check_input(arg.input)
+
+    names, data = read_csv(arg.input)
     if arg.kind is not None:
         arg.kind = Path(arg.kind).absolute().resolve()
         check_input(arg.kind)
+        name_kind, kinds = read_kind_csv(arg.kind ,names)
+    else:
+        name_kind, kinds = dict(), []
 
-    names, data = read_csv(arg.input)
     read_time = timer()
     r_limit = sys.getrecursionlimit()
     new_limit = len(names) * 10
@@ -268,8 +276,8 @@ def get_tree():
                             [e_dist_matrix, h_dist_matrix, s_dist_matrix]):
         matrix2csv(m_name, names, matrix, arg)
         if arg.kind is not None:
-            kinds, kind_mean_matrix, kind_std_matrix = matrix_to_kinds(
-                names, matrix, arg.kind)
+            kind_mean_matrix, kind_std_matrix = matrix_to_kinds(
+                names, matrix, name_kind, kinds)
             m2_name = f'{m_name}-kind_mean'
             m3_name = f'{m_name}-kind_std'
             matrix2csv(m2_name, kinds, kind_mean_matrix, arg)
@@ -281,9 +289,6 @@ def get_tree():
             executor.submit(build_nj_tree,m_name, names, matrix, arg)
             # build_nj_tree(m_name, names, matrix, arg)
     end = timer()
-    if arg.kind is not None:
-        arg.kind = Path(arg.kind).absolute().resolve()
-        check_input(arg.kind)
     log.info(f'{len(names)} samples')
     log.info(f'{len(data)*(len(data)-1)/2} pairs')
     log.info(f'Total time elapsed: {end - start:.2f}')
