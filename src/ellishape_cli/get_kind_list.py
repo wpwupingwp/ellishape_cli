070de6fec33257fb204a6d8c020f679a14de01a2
Ping Wu <wpwupingwp@outlook.com>
2025-02-12 19:25:38 +0800

    generate kind list from input

---
+++ src/ellishape_cli/get_kind_list.py
@@ -0,0 +1,19 @@
+from sys import argv
+import numpy as np
+from pathlib import Path
+
+input_file = Path(argv[1])
+a = np.loadtxt(input_file, dtype=str, delimiter=',',quotechar='"', skiprows=1)
+# np.random.shuffle(a)
+idx = np.arange(a.shape[0])+1
+
+# D:\\dataset\\hs_2024419\\Castanea sativa\\Castanea sativa Mill._730884840_13.png
+# change code according to format
+a1 = np.strings.replace(a[:, 0], '\\', '_')
+a2 = np.strings.replace(a1, 'D:_dataset_hs_2024419_', '')
+a3 = np.strings.partition(a2, '_')[0]
+
+out = np.stack([idx, a3]).T
+a[:, 0] = idx
+np.savetxt(input_file.with_suffix('.simple.csv'), a, delimiter=',', fmt='%s')
+np.savetxt(input_file.with_suffix('.kind.csv'), out, delimiter=',', fmt='%s')
