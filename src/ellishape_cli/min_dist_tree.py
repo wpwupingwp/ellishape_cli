509efc4a91de0ce12b791977734394fcc35beb15
Ping Wu <wpwupingwp@outlook.com>
2025-03-02 22:31:47 +0800

    after rotate 0 offset has min dist?

--- src/ellishape_cli/min_dist_tree.py
+++ src/ellishape_cli/min_dist_tree.py
@@ -10,7 +10,7 @@ from pathlib import Path
 from timeit import default_timer as timer
 
 
-def min_shape_distance_old(phi, A_dots, B_efd, n_dots):
+def min_dist_on_angle_old(phi, A_dots, B_efd, n_dots):
     # slow due to get dots from efd
     B_efd_rotated = rotate_efd(B_efd, phi.item())
     B_a2, B_b2, B_c2, B_d2 = np.hsplit(B_efd_rotated, 4)
@@ -21,7 +21,7 @@ def min_shape_distance_old(phi, A_dots, B_efd, n_dots):
     return diff
 
 
-def min_shape_distance_old2(phi, A_dots, B_dots):
+def min_dist_on_angle_old2(phi, A_dots, B_dots):
     # slow due to pdist and squareform
     # n_dots * n_samples
     B_dots_rotated = rotate_dots(B_dots, phi.item())
@@ -31,13 +31,40 @@ def min_shape_distance_old2(phi, A_dots, B_dots):
     return diff
 
 
-def min_shape_distance(phi, A_dots, B_dots):
+def min_dist_on_angle(phi, A_dots, B_dots):
+    # only consider angle
     B_dots_rotated = rotate_dots(B_dots, phi.item())
     # n_dots * n_samples
     m, n = B_dots.shape
     factor = np.sqrt(1/(m))
     diff = np.linalg.norm(A_dots.ravel()-B_dots_rotated.ravel()) * factor
+    # A2 = A_dots.ravel()
+    # B2 = B_dots_rotated.ravel()
+    # for i in range(m):
+    #     new = np.roll(B2, i)
+    #     print(B2.shape, new.shape)
+    #     diff2 = np.linalg.norm(A2-new) * factor
+    #     print(diff, diff2)
     return diff
+
+
+def min_dist_on_offset(A_dots, B_dots):
+    # only consider offset
+    a = timer()
+    A_ = A_dots.ravel()
+    B_ = B_dots.ravel()
+    # n_dots
+    n = A_dots.shape[0]
+    # (x, y)
+    m = n * 2
+    B_roll_matrix = np.empty((m, m))
+    for i in range(0, n):
+        B_roll_matrix[i] = np.roll(B_, -i*2)
+    dist = np.linalg.norm(A_-B_roll_matrix, axis=1)
+    offset = np.argmin(dist)
+    b = timer()
+    log.warning(f'Min dist on offset cost {b-a:.6f} seconds')
+    return offset, dist[offset]
 
 
 def min_shape_distance2(phi, A_dots, B_dots):
@@ -128,13 +155,13 @@ def rotate_dots(dots: np.ndarray, angle: float):
 def calibrate(A_dots, B_dots, method='Powell'):
     x0 = np.array([0])
     if method == 'Bounded':
-        result = minimize_scalar(min_shape_distance, args=(A_dots, B_dots),
-                                  method='Bounded',
-                                  bounds=(0, np.pi*2))
+        result = minimize_scalar(min_dist_on_angle, args=(A_dots, B_dots),
+                                 method='Bounded',
+                                 bounds=(0, np.pi*2))
     else:
-        result = minimize(min_shape_distance, x0, args=(A_dots, B_dots),
-                           method=method,
-                           bounds=[(0, np.pi*2)])
+        result = minimize(min_dist_on_angle, x0, args=(A_dots, B_dots),
+                          method=method,
+                          bounds=[(0, np.pi*2)])
     return result
 
 
@@ -151,10 +178,10 @@ def calibrate2(A_dots, B_efd, n_dots, method='Powell'):
     return result2
 
 
-def use_brute(A_dots, B_dots, B_efd, n_dots):
+def use_brute(A_dots, B_dots):
     start = timer()
     # brute_result = brute(min_shape_distance_old, args=(A_dots, B_efd, n_dots), Ns=360,
-    brute_result = brute(min_shape_distance, args=(A_dots, B_dots), Ns=360,
+    brute_result = brute(min_dist_on_angle, args=(A_dots, B_dots), Ns=360,
                          ranges=[(0, np.pi * 2)], workers=8, full_output=True)
     end = timer()
     log.warning(f'Brute force on angle cost {end-start:.6f} seconds')
@@ -163,32 +190,53 @@ def use_brute(A_dots, B_dots, B_efd, n_dots):
     log.debug(y_result)
     phi = x_result[0].item()
     dist = y_result
-    rotated_efd = rotate_efd(B_efd, phi)
-    B_a2, B_b2, B_c2, B_d2 = np.hsplit(rotated_efd, 4)
-    dots2_2 = get_curve_from_efd(B_a2, B_b2, B_c2, B_d2, B_a2.shape[0], n_dots)
-    dots2_3 = rotate_dots(B_dots, phi)
-    log.info(f'\trotated efd->dots vs rotated dots: {np.sum(dots2_2-dots2_3)}')
     log.info(f'\tRotate B -{np.rad2deg(np.pi*2-phi):.6f}\u00b0')
     log.info(f'\tMinimize distance {dist:.6f}')
+    return phi, dist, brute_result
 
-    fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(20, 10))
-    ax1.set_title('Distance')
-    ax2.set_title('Shape')
-    ax1.plot(x_list, y_list)
-    ax1.plot(x_result, y_result, 'ro')
-    ax1.set_xticks(np.arange(0, 9) * np.pi / 4,
+
+def plot(brute_result, A_dots, B_dots, B_dots2, phi, n_dots):
+    x_result, y_result, x_list, y_list = brute_result
+
+    B_dots3 = rotate_dots(B_dots, phi)
+    log.info(f'rotated efd->dots vs rotated dots: {np.sum(B_dots2-B_dots3)}')
+
+    fig, axs = plt.subplots(2, 2, figsize=(20, 20))
+    axs[0, 0].set_title('Angle-Distance (offset=0)')
+    axs[0, 1].set_title('Shape')
+    axs[1, 0].set_title('Offset-Distance (angle=0)')
+    axs[1, 1].set_title('Offset-Distance (angle=best)')
+    axs[0, 0].plot(x_list, y_list)
+    axs[0, 0].plot(x_result, y_result, 'ro')
+    axs[0, 0].set_xticks(np.arange(0, 9) * np.pi / 4,
                    [rf'$\frac{{{i}\pi}}{{4}}$' for i in range(9)])
-    ax1.set_xlabel('Degree')
-    ax1.set_ylabel('Euclidean distance')
+    axs[0, 0].set_xlabel('Degree')
+    axs[0, 0].set_ylabel('Euclidean distance')
 
-    ax2.plot(A_dots[:, 0], A_dots[:, 1], 'b', label='A', linewidth=2, alpha=0.8)
-    ax2.plot(B_dots[:, 0], B_dots[:, 1], 'g-.', label='B')
-    ax2.plot(dots2_2[:, 0], dots2_2[:, 1], 'r-', label='B rotated', linewidth=2,
+    axs[0, 1].plot(A_dots[:, 0], A_dots[:, 1], 'b', label='A', linewidth=2, alpha=0.8)
+    axs[0, 1].plot(B_dots[:, 0], B_dots[:, 1], 'g-.', label='B')
+    axs[0, 1].plot(B_dots2[:, 0], B_dots2[:, 1], 'r-', label='B rotated', linewidth=2,
              alpha=0.8)
-    ax2.legend()
-    ax2.set_aspect('equal')
+    axs[0, 1].legend()
+    axs[0, 1].set_aspect('equal')
+
+    x_ = np.arange(n_dots)
+    y3 = [np.linalg.norm(A_dots.ravel()-np.roll(B_dots.ravel(), -i*2)) for i in range(n_dots)]
+    y4 = [np.linalg.norm(A_dots.ravel()-np.roll(B_dots2.ravel(), -i*2)) for i in range(n_dots)]
+    x3_min, y3_min = min_dist_on_offset(A_dots, B_dots)
+    x4_min, y4_min = min_dist_on_offset(A_dots, B_dots2)
+    # axs[1, 0].plot(x3, y3, 'ro', linewidth=1)
+    # axs[1, 1].plot(x3, y4, 'go', linewidth=1)
+    axs[1, 0].plot(x_, y3)
+    axs[1, 0].plot(x3_min, y3_min, 'ro', label=f'{x3_min}, {y3_min}')
+    axs[1, 1].plot(x_, y4)
+    axs[1, 1].plot(x4_min, y4_min, 'ro', label=f'{x4_min}, {y4_min}')
+    axs[1, 0].set_xlabel('Dot offset')
+    axs[1, 1].set_xlabel('Dot offset')
+    axs[1, 0].set_ylabel('Distance')
+    axs[1, 1].set_ylabel('Distance')
     plt.show()
-    return phi, dist
+    return
 
 
 def main():
@@ -201,8 +249,8 @@ def main():
 
     n_dots = 256
     A_efd = data[0].reshape(-1, 4).astype(np.float64)
-    # B_efd = data[1].reshape(-1, 4).astype(np.float64)
-    B_efd = A_efd.copy()
+    B_efd = data[1].reshape(-1, 4).astype(np.float64)
+    # B_efd = A_efd.copy()
 
     deg = 90.005
     rad = np.deg2rad(deg)
@@ -214,9 +262,11 @@ def main():
     B_a, B_b, B_c, B_d = np.hsplit(B_efd, 4)
     B_dots = get_curve_from_efd(B_a, B_b, B_c, B_d, B_a.shape[0], n_dots)
 
-    # min_shape_distance2(np.array([rad]), A_dots, B_dots)
+    min_dist_on_angle(np.array([rad]), A_dots, B_dots)
+    min_dist_on_offset(A_dots, B_dots)
 
-    phi, dist = use_brute(A_dots, B_dots, B_efd, n_dots)
+    phi, dist, brute_result = use_brute(A_dots, B_dots)
+    B_dots_rotated = rotate_dots(B_dots, phi)
     # log.info(f'Brute result: {phi}, {dist}')
     for m in ('Bounded', 'Powell'):
         # for m in ('Powell',):
@@ -238,6 +288,7 @@ def main():
             log.error(e)
             continue
         # log.info(result2)
+    plot(brute_result, A_dots, B_dots, B_dots_rotated, phi, n_dots)
     log.info(f'Rotate before vs after: '
              f'{np.sum(B_efd - rotate_efd(B_efd, -deg))}')
     return
